61df67b6b7faac62b7298ef15e55aa2d86caccc1
leahfeuer <leah.s.feuer@gmail.com>
2014-03-30 09:39:25 -0400

    fixed data attributes so active class appears

--- CHTjavascript.js
+++ CHTjavascript.js
@@ -5,10 +5,24 @@ $( document ).ready(function() {
 
 });
 
-var height = $(window).height(); 
-$(".full-height").css( "height" , height);
 
+var contentHeight = $(".content").height();
+var fullHeight = $(window).height(); 
+if(fullHeight>contentHeight) {
+$(".full-height").css( "height" , fullHeight);
+}
 
+/*
+{
+  // specify the class used for active sections
+  active_class: 'active',
+  // how many pixels until the magellan bar sticks, 0 = auto
+  threshold: 0,
+  // pixels from the top of destination for it to be considered active
+  destination_threshold: 20,
+  // calculation throttling to increase framerate
+  throttle_delay: 50 
+}*/
 
 /*(window).bind("load", function () {
     var footer = $("#footer");
