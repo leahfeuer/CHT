7547139d34b147ef9aa1949ff1b487d26705e8ed
leahfeuer <leah.s.feuer@gmail.com>
2014-04-26 09:43:09 -0400

    added google map styled

--- CHTjavascript.js
+++ CHTjavascript.js
@@ -45,10 +45,40 @@ var navcolor = function (){
     }
   };
 
+function initialize() {
+  var styles =[
+  {
+    stylers:[
+      {hue: '#9ECFCA'}
+    ]
+  }];
+
+   // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var mapOptions = {
+    center: new google.maps.LatLng(40.6932942,-73.9908107), 
+    zoom: 17
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), 
+    mapOptions);
+
+//Associate the styled map with the MapTypeId and set it to display.
+
+    map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
+  };
+  
+
 
 $(document).ready( function (){
   divheight ();
   navcolor ();
+  google.maps.event.addDomListener(window, 'load', initialize);
 });
 
 $(window).resize( function (){
